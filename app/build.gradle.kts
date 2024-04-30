41332b04444e5a71f57e9225387d1bb913b20f86
bin0o <malbino2002@gmail.com>
2024-04-30 01:57:25 +0100

    feat: added search and current location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -68,4 +69,7 @@ dependencies {
 
     // Maps SDK for Android
     implementation(libs.play.services.maps)
+
+    // Location SDK for Android
+    implementation(libs.play.services.location)
 }
