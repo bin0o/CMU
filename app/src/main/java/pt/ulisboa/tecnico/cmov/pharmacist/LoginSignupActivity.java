375df6513d5048e9139058515c22b68e31526f08
Matilde Tocha <matilde.tocha@tecnico.ulisboa.pt>
2024-05-02 15:07:37 +0100

    feat: Login/ Signup views

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/LoginSignupActivity.java
@@ -0,0 +1,57 @@
+package pt.ulisboa.tecnico.cmov.pharmacist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class LoginSignupActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
+        viewPager.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/LoginTabFragment.java
@@ -0,0 +1,39 @@
+package pt.ulisboa.tecnico.cmov.pharmacist;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginTabFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_login_tab, container, false);
+
+        EditText username = view.findViewById(R.id.login_username);
+        EditText password = view.findViewById(R.id.login_password);
+
+        //TODO look for credentials in the DB
+
+        Button login_button = view.findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
