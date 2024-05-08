66b605040cf3478793223baefbebda065650fc49
bin0o <malbino2002@gmail.com>
2024-05-08 01:08:01 +0100

    feat (AddPharmacy): now adding pharmacies recursively

--- app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/AddPharmacyActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/AddPharmacyActivity.java
@@ -8,8 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.ComponentActivity;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -46,10 +49,21 @@ public class AddPharmacyActivity extends ComponentActivity {
                  EditText pharmacyAddressEditText = findViewById(R.id.location);
                  String pharmacyAddress = pharmacyAddressEditText.getText().toString();
 
+                 // Create a new Pharmacy object
                  Pharmacy pharmacy = new Pharmacy(pharmacyName, pharmacyAddress);
 
-                 mDatabase.child("Pharmacy").child("1").child("name").setValue(pharmacyName);
-                 mDatabase.child("Pharmacy").child("1").child("address").setValue(pharmacyAddress);
+                 // Push the Pharmacy object to the "Pharmacy" node
+                 mDatabase.child("Pharmacy").push().setValue(pharmacy)
+                         .addOnCompleteListener(new OnCompleteListener<Void>() {
+                             @Override
+                             public void onComplete(@NonNull Task<Void> task) {
+                                 if (task.isSuccessful()) {
+                                     Log.d(TAG, "addPharmacy: Pharmacy added successfully");
+                                 } else {
+                                     Log.e(TAG, "addPharmacy: Failed to add pharmacy", task.getException());
+                                 }
+                             }
+                         });
 
                  Log.i(TAG, "Going back to Home Page");
                  Intent intent = new Intent(AddPharmacyActivity.this, HomePageActivity.class);
--- app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/Pharmacy.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/Pharmacy.java
@@ -4,14 +4,15 @@ public class Pharmacy {
     private String name;
     private String address;
 
+    // Required empty constructor for Firebase
     public Pharmacy() {}
 
     public Pharmacy(String name, String address) {
         this.name = name;
         this.address = address;
-
     }
 
+    // Getter and setter methods
     public String getName() {
         return name;
     }
@@ -28,3 +29,5 @@ public class Pharmacy {
         this.address = address;
     }
 }
+
+
